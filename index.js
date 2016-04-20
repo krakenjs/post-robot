1b2eb79ffec6bd501f6919bbb4c3254be9e6d879
Daniel Brain <dbrain@paypal.com>
2016-04-20 10:36:43 -0700

    Remove AMD export

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 (function (root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define([], factory);
-    } else if (typeof exports === "object") {
+    if (typeof exports === "object") {
         module.exports = factory();
     } else {
         root.postRobot = factory();
