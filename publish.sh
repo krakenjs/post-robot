2989311b68fa27248504061dc51ad56d0c49c73b
Daniel Brain <dbrain@paypal.com>
2016-04-28 17:50:52 -0700

    Update publish script

--- publish.sh
+++ publish.sh
@@ -7,8 +7,7 @@ gulp build;
 git add dist;
 git commit -m "Dist" || echo "Nothing to distribute";
 
-bower version patch;
-npm version from-git;
+mversion patch -m '%s';
 
 git push;
 git push --tags;
