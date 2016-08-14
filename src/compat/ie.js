3e4b930c1454db79a27c3cb2d076b371c466d270
Daniel Brain <dbrain@paypal.com>
2016-08-14 12:55:01 -0700

    Allow post messages between two windows on the same domain

--- src/compat/ie.js
+++ src/compat/ie.js
@@ -1,9 +1,13 @@
 
 import { CONFIG } from '../conf';
-import { isSameTopWindow } from '../lib';
+import { isSameTopWindow, isSameDomain } from '../lib';
 
 export function emulateIERestrictions(sourceWindow, targetWindow) {
     if (!CONFIG.ALLOW_POSTMESSAGE_POPUP) {
+
+        if (isSameDomain(sourceWindow) && isSameDomain(targetWindow)) {
+            return;
+        }
 
         if (!isSameTopWindow(sourceWindow, targetWindow)) {
             throw new Error(`Can not send and receive post messages between two different windows (disabled to emulate IE)`);
