976b8d381fec4961e18d41f77c7ef32d279829c1
Daniel Brain <dbrain@paypal.com>
2016-09-27 21:57:19 -0700

    Dist

--- dist/post-robot.js
+++ dist/post-robot.js
@@ -396,12 +396,12 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    ALLOW_POSTMESSAGE_POPUP: false,
 
-	    LOG_LEVEL: 'debug',
+	    LOG_LEVEL: 'info',
 
 	    BRIDGE_TIMEOUT: 5000,
 	    ACK_TIMEOUT: 1000,
 
-	    LOG_TO_PAGE: true,
+	    LOG_TO_PAGE: false,
 
 	    MOCK_MODE: false,
 
