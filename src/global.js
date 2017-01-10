dfc85a86a33b86cdb9f8dc6c8bd9d4445fda91be
Daniel Brain <dbrain@paypal.com>
2017-01-10 14:04:36 -0800

    Add registerSelf noop for backwards compatibility

--- src/global.js
+++ src/global.js
@@ -5,3 +5,9 @@ import { cleanup } from './lib/cleanup';
 export let global = window[CONSTANTS.WINDOW_PROPS.POSTROBOT] = window[CONSTANTS.WINDOW_PROPS.POSTROBOT] || {};
 
 global.clean = global.clean || cleanup(global);
+
+// Backwards compatibility
+
+global.registerSelf = () => {
+    // pass
+};
