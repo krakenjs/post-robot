fa97e489428b77a0a4498697f8f8a7347426bf57
Daniel Brain <dbrain@paypal.com>
2017-04-03 19:16:54 -0700

    Make karma not depend on lint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -127,7 +127,7 @@ gulp.task('lint', function() {
   .pipe(eslint.failAfterError());
 });
 
-gulp.task('karma', ['lint'], function (done) {
+gulp.task('karma', function (done) {
 
     var server = new Server({
         configFile: __dirname + '/karma.conf.js',
