fe592e875de939bd97c846bb1bc877e9a079360a
Daniel Brain <dbrain@paypal.com>
2017-04-24 10:21:42 -0700

    Prevent publish with uncommited changes

--- publish.sh
+++ publish.sh
@@ -2,6 +2,16 @@
 
 set -e;
 
+if ! git diff-files --quiet; then
+    echo "Can not publish with unstaged uncommited changes";
+    exit 1;
+fi;
+
+if ! git diff-index --quiet --cached HEAD; then
+    echo "Can not publish with staged uncommited changes";
+    exit 1;
+fi;
+
 rm -rf node_modules/cross-domain-safe-weakmap node_modules/sync-browser-mocks
 npm install cross-domain-safe-weakmap sync-browser-mocks
 
