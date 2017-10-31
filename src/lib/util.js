87aad8287cde15799b1b4dec783e16ccdb4095e7
Daniel Brain <dbrain@paypal.com>
2017-10-30 21:57:17 -0700

    Make stringifyError more resilient

--- src/lib/util.js
+++ src/lib/util.js
@@ -5,38 +5,47 @@ import { isPopup, isIframe } from 'cross-domain-utils/src';
 import { CONSTANTS } from '../conf';
 
 
-export function stringifyError(err : mixed) : string {
+export function stringifyError(err : mixed, level : number = 1) : string {
 
-    if (!err) {
-        return `<unknown error: ${Object.prototype.toString.call(err)}>`;
+    if (level >= 3) {
+        return 'stringifyError stack overflow';
     }
 
-    if (typeof err === 'string') {
-        return err;
-    }
-
-    if (err instanceof Error) {
-        let stack = err && err.stack;
-        let message = err && err.message;
-
-        if (stack && message) {
-            if (stack.indexOf(message) !== -1) {
-                return stack;
-            } else {
-                return `${message}\n${stack}`;
-            }
-        } else if (stack) {
-            return stack;
-        } else if (message) {
-            return message;
+    try {
+        if (!err) {
+            return `<unknown error: ${Object.prototype.toString.call(err)}>`;
         }
-    }
 
-    if (typeof err.toString === 'function') {
-        return err.toString();
-    }
+        if (typeof err === 'string') {
+            return err;
+        }
 
-    return Object.prototype.toString.call(err);
+        if (err instanceof Error) {
+            let stack = err && err.stack;
+            let message = err && err.message;
+
+            if (stack && message) {
+                if (stack.indexOf(message) !== -1) {
+                    return stack;
+                } else {
+                    return `${message}\n${stack}`;
+                }
+            } else if (stack) {
+                return stack;
+            } else if (message) {
+                return message;
+            }
+        }
+
+        if (typeof err.toString === 'function') {
+            return err.toString();
+        }
+
+        return Object.prototype.toString.call(err);
+
+    } catch (newErr) {
+        return `Error while stringifying error: ${ stringifyError(newErr, level + 1) }`;
+    }
 }
 
 // eslint-disable-next-line flowtype/no-weak-types
