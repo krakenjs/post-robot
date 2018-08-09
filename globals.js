e12dca39c383eb969d2ba23f0f5aea627ddccf15
Daniel Brain <dbrain@paypal.com>
2018-08-08 23:23:09 -0700

    Remove erroneous global

--- globals.js
+++ globals.js
@@ -6,6 +6,5 @@ module.exports = {
         __IE_POPUP_SUPPORT__:        true,
         __ALLOW_POSTMESSAGE_POPUP__: true,
         __GLOBAL_MESSAGE_SUPPORT__:  true
-    },
-    '__POST_ROBOT__.__IE_POPUP_SUPPORT__': true
+    }
 };
