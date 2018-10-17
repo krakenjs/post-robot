d0c188be84c821ab6901f9312905fb490dbeda2c
Daniel Brain <dbrain@paypal.com>
2018-10-17 10:38:01 -0700

    Dist

--- dist/post-robot.js
+++ dist/post-robot.js
@@ -176,7 +176,7 @@
                 if (!(instance instanceof Constructor)) throw new TypeError("Cannot call a class as a function");
             }
             function htmlEncode() {
-                return (arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "").toString().replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;").replace(/\//g, "&#x2F;");
+                return (arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "").toString().replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#x27;").replace(/\//g, "&#x2F;");
             }
             !function(_JsxHTMLNode) {
                 !function(subClass, superClass) {
@@ -543,18 +543,18 @@
                     counter += 1;
                     this.name = "__weakmap_" + (1e9 * Math.random() >>> 0) + "__" + counter;
                     if (function() {
-                        if (!window.WeakMap) return !1;
-                        if (!window.Object.freeze) return !1;
+                        if ("undefined" == typeof WeakMap) return !1;
+                        if (void 0 === Object.freeze) return !1;
                         try {
-                            var testWeakMap = new window.WeakMap(), testKey = {};
-                            window.Object.freeze(testKey);
+                            var testWeakMap = new WeakMap(), testKey = {};
+                            Object.freeze(testKey);
                             testWeakMap.set(testKey, "__testvalue__");
                             return "__testvalue__" === testWeakMap.get(testKey);
                         } catch (err) {
                             return !1;
                         }
                     }()) try {
-                        this.weakmap = new window.WeakMap();
+                        this.weakmap = new WeakMap();
                     } catch (err) {}
                     this.keys = [];
                     this.values = [];
