d3bde671c6e698eee55e4daed298fe05b562860d
Daniel Brain <daniel@bluesuncorp.co.uk>
2018-11-21 17:41:34 -0800

    Export serializeMessage and deserializeMessage

--- src/interface.js
+++ src/interface.js
@@ -5,7 +5,7 @@ import { listenForMessages } from './drivers';
 import { global } from './global';
 
 export * from './public';
-export { markWindowKnown } from './lib';
+export { markWindowKnown, serializeMessage, deserializeMessage } from './lib';
 export { cleanUpWindow } from './clean';
 export { ZalgoPromise as Promise } from 'zalgo-promise/src';
 export let bridge = __POST_ROBOT__.__IE_POPUP_SUPPORT__ ? require('./bridge/interface') : null;
