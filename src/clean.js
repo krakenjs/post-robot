9564a44589447e89dfc0d79a7f781447766369c8
Daniel Brain <dbrain@paypal.com>
2018-12-24 23:17:33 +0000

    Asynchronously reject window promises

--- src/clean.js
+++ src/clean.js
@@ -1,11 +1,12 @@
 /* @flow */
 
 import { type CrossDomainWindowType } from 'cross-domain-utils/src';
+import { noop } from 'belter/src';
 
 import { requestPromises } from './public';
 
 export function cleanUpWindow(win : CrossDomainWindowType) {
     for (let promise of requestPromises.get(win, [])) {
-        promise.reject(new Error(`Window cleaned up before response`));
+        promise.reject(new Error(`Window cleaned up before response`)).catch(noop);
     }
 }
