1a303ed26f4fa120d7927befe3a715526d4c1ba7
Daniel Brain <daniel@brainblocks.io>
2019-02-07 21:39:03 -0800

    Get version for globals inline

--- globals.js
+++ globals.js
@@ -1,13 +1,15 @@
 /* @flow */
 /* eslint import/no-commonjs: 0 */
 
-const { getCurrentVersion } = require('grumbler-scripts/config/webpack.config');
-
 const pkg = require('./package.json');
+
+const formatVersion = (version) => {
+    return version.replace(/[^\d]+/g, '_');
+};
 
 module.exports = {
     __POST_ROBOT__: {
-        __GLOBAL_KEY__:              `__post_robot_${ getCurrentVersion(pkg) }__`,
+        __GLOBAL_KEY__:              `__post_robot_${ formatVersion(pkg.version) }__`,
         __AUTO_SETUP__:              true,
         __IE_POPUP_SUPPORT__:        true,
         __GLOBAL_MESSAGE_SUPPORT__:  true
