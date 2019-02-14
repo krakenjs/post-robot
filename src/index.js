de90fd13a7acf67a6f9c0742881c1b741288bd95
Daniel Brain <dbrain@paypal.com>
2019-02-14 15:41:15 -0800

    Export destroy function

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export { ZalgoPromise as Promise } from 'zalgo-promise/src';
 
 export * from './types';
 export { ProxyWindow } from './serialize';
-export { setup, serializeMessage, deserializeMessage, toProxyWindow } from './setup';
+export { setup, destroy, serializeMessage, deserializeMessage, toProxyWindow } from './setup';
 export { on, once, send } from './public';
 export { markWindowKnown } from './lib';
 export { cleanUpWindow } from './clean';
