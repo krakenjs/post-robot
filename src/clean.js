49d6268ff0519096dbf7724cf5e83eba5c8f8594
Daniel Brain <daniel@brainblocks.io>
2020-01-22 23:48:01 -0800

    Improve clean up window message

--- src/clean.js
+++ src/clean.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import { type CrossDomainWindowType } from 'cross-domain-utils/src';
+import { type CrossDomainWindowType, isWindowClosed } from 'cross-domain-utils/src';
 import { noop } from 'belter/src';
 
 import { windowStore } from './global';
@@ -8,6 +8,6 @@ import { windowStore } from './global';
 export function cleanUpWindow(win : CrossDomainWindowType) {
     const requestPromises = windowStore('requestPromises');
     for (const promise of requestPromises.get(win, [])) {
-        promise.reject(new Error(`Window cleaned up before response`)).catch(noop);
+        promise.reject(new Error(`Window ${ isWindowClosed(win) ? 'closed' : 'cleaned up' } before response`)).catch(noop);
     }
 }
