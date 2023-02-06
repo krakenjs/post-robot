c5b70a53cdeaab880c8319ec6893479b17f09d5f
Pavlo-Bohdan Ravliv <pravliv@atlassian.com>
2023-02-06 16:56:11 +0100

    feat: add check if listener exists

---
+++ src/public/listenerExists.js
@@ -0,0 +1,34 @@
+/* @flow */
+
+import { getRequestListener } from "../drivers";
+import type { ServerOptionsType } from "../types";
+
+const getDefaultServerOptions = (): ServerOptionsType => {
+  // $FlowFixMe
+  return {};
+};
+
+export function listenerExists(
+  name: string,
+  options: ?ServerOptionsType
+): boolean {
+  if (!name) {
+    throw new Error("Expected name");
+  }
+
+  options = options || getDefaultServerOptions();
+
+  const win = options.window;
+  const domain = options.domain;
+
+  if (Array.isArray(domain)) {
+    for (const item of domain) {
+      if (getRequestListener({ name, win, domain: item })) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  return Boolean(getRequestListener({ name, win, domain }));
+}
