79086c4caac14a1bc609881f94e27406c58dd5f5
Brian Tedder <btedder@paypal.com>
2024-08-05 09:11:22 -0500

    formatting

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = require('./dist/post-robot');
 
-
 // eslint-disable-next-line import/no-commonjs
 module.exports.default = module.exports;
