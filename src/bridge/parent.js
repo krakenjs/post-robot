2daff1aa9c080f007c8cbc42e97082f1f2682c11
Brian Tedder <btedder@paypal.com>
2024-08-05 10:46:42 -0500

    fix: remove setAttribute('style', ...) from openBridgeFrame (#121)
    * fix: remove setAttribute style from openBridge

--- src/bridge/parent.js
+++ src/bridge/parent.js
@@ -2,7 +2,13 @@
 
 import { WeakMap } from 'cross-domain-safe-weakmap/src';
 import { ZalgoPromise } from 'zalgo-promise/src';
-import { getDomain, getFrameByName, isWindowClosed, getDomainFromUrl, type CrossDomainWindowType } from 'cross-domain-utils/src';
+import {
+    getDomain,
+    getFrameByName,
+    isWindowClosed,
+    getDomainFromUrl,
+    type CrossDomainWindowType
+} from 'cross-domain-utils/src';
 
 import { CONFIG, CONSTANTS } from '../conf';
 import { onChildWindowReady } from '../lib';
@@ -18,7 +24,6 @@ global.popupWindowsByName = global.popupWindowsByName || {};
 
 function listenForRegister(source, domain) {
     global.on(CONSTANTS.POST_MESSAGE_NAMES.OPEN_TUNNEL, { window: source, domain }, ({ origin, data }) => {
-
         if (origin !== domain) {
             throw new Error(`Domain ${ domain } does not match origin ${ origin }`);
         }
@@ -40,14 +45,17 @@ function listenForRegister(source, domain) {
         }
 
         if (global.popupWindowsByName[data.name].domain !== origin) {
-            throw new Error(`Message origin ${ origin } does not matched registered window origin ${ global.popupWindowsByName[data.name].domain }`);
+            throw new Error(
+                `Message origin ${ origin } does not matched registered window origin ${
+                    global.popupWindowsByName[data.name].domain
+                }`
+            );
         }
 
         registerRemoteSendMessage(global.popupWindowsByName[data.name].win, domain, data.sendMessage);
 
         return {
             sendMessage(message) {
-
                 if (!window || window.closed) {
                     return;
                 }
@@ -71,15 +79,39 @@ function listenForRegister(source, domain) {
         };
     });
 }
+function setElementStyles(
+    element : HTMLElement,
+    styles : {
+    // eslint-disable-next-line no-undef
+    [key : $Keys<CSSStyleDeclaration>] : $Values<CSSStyleDeclaration>
+  } = {}
+) {
+    if (!element) {
+        return;
+    }
+
+    for (const key in styles) {
+    // $FlowFixMe
+        if (Object.prototype.hasOwnProperty.call(styles, key)) {
+            // $FlowFixMe
+            element.style[key] = styles[key];
+        }
+    }
+}
 
 function openBridgeFrame(name : string, url : string) : HTMLIFrameElement {
-
     let iframe = document.createElement(`iframe`);
 
     iframe.setAttribute(`name`, name);
-    iframe.setAttribute(`id`,   name);
+    iframe.setAttribute(`id`, name);
+    setElementStyles(iframe, {
+        display:  'none',
+        margin:   0,
+        padding:  0,
+        border:   '0px none',
+        overflow: 'hidden'
+    });
 
-    iframe.setAttribute(`style`, `display: none; margin: 0; padding: 0; border: 0px none; overflow: hidden;`);
     iframe.setAttribute(`frameborder`, `0`);
     iframe.setAttribute(`border`, `0`);
     iframe.setAttribute(`scrolling`, `no`);
@@ -101,7 +133,6 @@ export function hasBridge(url : string, domain : string) : boolean {
 }
 
 export function openBridge(url : string, domain : string) : ZalgoPromise<CrossDomainWindowType> {
-
     domain = domain || getDomainFromUrl(url);
 
     if (global.bridges[domain]) {
@@ -109,12 +140,11 @@ export function openBridge(url : string, domain : string) : ZalgoPromise<CrossDo
     }
 
     global.bridges[domain] = ZalgoPromise.try(() => {
-
         if (getDomain() === domain) {
             throw new Error(`Can not open bridge on the same domain as current domain: ${ domain }`);
         }
 
-        let name  = getBridgeName(domain);
+        let name = getBridgeName(domain);
         let frame = getFrameByName(window, name);
 
         if (frame) {
@@ -124,8 +154,7 @@ export function openBridge(url : string, domain : string) : ZalgoPromise<CrossDo
         let iframe = openBridgeFrame(name, url);
         global.bridgeFrames[domain] = iframe;
 
-        return documentBodyReady.then(body => {
-
+        return documentBodyReady.then((body) => {
             body.appendChild(iframe);
 
             let bridge = iframe.contentWindow;
@@ -133,18 +162,15 @@ export function openBridge(url : string, domain : string) : ZalgoPromise<CrossDo
             listenForRegister(bridge, domain);
 
             return new ZalgoPromise((resolve, reject) => {
-
                 iframe.onload = resolve;
                 iframe.onerror = reject;
-
-            }).then(() => {
-
-                return onChildWindowReady(bridge, CONFIG.BRIDGE_TIMEOUT, `Bridge ${ url }`);
-
-            }).then(() => {
-
-                return bridge;
-            });
+            })
+                .then(() => {
+                    return onChildWindowReady(bridge, CONFIG.BRIDGE_TIMEOUT, `Bridge ${ url }`);
+                })
+                .then(() => {
+                    return bridge;
+                });
         });
     });
 
@@ -154,7 +180,6 @@ export function openBridge(url : string, domain : string) : ZalgoPromise<CrossDo
 let windowOpen = window.open;
 
 window.open = function windowOpenWrapper(url : string, name : string, options : string, last : mixed) : mixed {
-
     let domain = url;
 
     if (url && url.indexOf(CONSTANTS.MOCK_PROTOCOL) === 0) {
@@ -182,8 +207,7 @@ window.open = function windowOpenWrapper(url : string, name : string, options : 
     }
 
     if (name && win) {
-        let winOptions = global.popupWindowsByWin.get(win) ||
-                         global.popupWindowsByName[name] || {};
+        let winOptions = global.popupWindowsByWin.get(win) || global.popupWindowsByName[name] || {};
 
         winOptions.name = winOptions.name || name;
         winOptions.win = winOptions.win || win;
@@ -197,7 +221,6 @@ window.open = function windowOpenWrapper(url : string, name : string, options : 
 };
 
 export function linkUrl(win : CrossDomainWindowType, url : string) {
-
     let winOptions = global.popupWindowsByWin.get(win);
 
     if (winOptions) {
